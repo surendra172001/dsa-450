85a9c44c42dee6d3e58a2de895ecff2b89067565
Surendra Pandey <sap172001@gmail.com>
2020-12-14 11:18:09 +0530

    Added 3 way partitioning method in the file

--- sort012.cpp
+++ sort012.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void sort012(int a[], int n)
+void countingMethod(int a[], int n)
 {
     int zeroCnt = 0;
     int oneCnt = 0;
@@ -27,7 +27,66 @@ void sort012(int a[], int n)
     }
 }
 
+void threeWayPartitioning(int a[], int n)
+{
+    int low = 0,
+        mid = 0,
+        high = n - 1;
+
+    while (mid <= high)
+    {
+        if (a[low] == 0)
+        {
+            low += 1;
+            mid = (low > mid ? low : mid);
+        }
+        else if (a[mid] == 1)
+        {
+            mid += 1;
+        }
+        else if (a[high] == 2)
+        {
+            high -= 1;
+        }
+        else if (a[mid] == 0)
+        {
+            swap(a[low], a[mid]);
+            low += 1;
+            mid += 1;
+        }
+        else if (a[mid] == 2)
+        {
+            swap(a[mid], a[high]);
+            high -= 1;
+        }
+    }
+}
+
+void sort012(int a[], int n)
+{
+    // countingMethod(a, n);
+    threeWayPartitioning(a, n);
+}
+
 int main()
 {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        sort012(arr, n);
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
