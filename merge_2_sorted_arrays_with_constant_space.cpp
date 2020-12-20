55850e0f99d2c36d4f238490590d3e881deac7d3
Surendra Pandey <sap172001@gmail.com>
2020-12-20 05:47:24 +0530

    Added merge 2 sorted arrays with constant space

---
+++ merge_2_sorted_arrays_with_constant_space.cpp
@@ -0,0 +1,117 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printArr(int arr[], int n)
+{
+    cout << n << ": ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+class Solution
+{
+public:
+    void mergeUsingInsertion(int arr1[], int arr2[], int m, int n)
+    {
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+
+            if (arr1[m - 1] > arr2[i])
+            {
+
+                swap(arr1[m - 1], arr2[i]);
+
+                int j = m - 1;
+
+                while (j > 0 && (arr1[j - 1] > arr1[j]))
+                {
+
+                    swap(arr1[j - 1], arr1[j]);
+                    j -= 1;
+                }
+            }
+        }
+    }
+
+    void iterateWithGapWidth(int arr1[], int arr2[], int n1, int n2, int gap)
+    {
+        int totalSize = n1 + n2;
+        int leftIdx = 0, rightIdx = 0 + gap;
+        while (rightIdx < totalSize)
+        {
+            if (rightIdx < n1)
+            {
+                if (arr1[leftIdx] > arr1[rightIdx])
+                {
+                    swap(arr1[leftIdx], arr1[rightIdx]);
+                }
+            }
+            else if (leftIdx < n1)
+            {
+                if (arr1[leftIdx] > arr2[rightIdx - n1])
+                {
+                    swap(arr1[leftIdx], arr2[rightIdx - n1]);
+                }
+            }
+            else
+            {
+                if (arr2[leftIdx - n1] > arr2[rightIdx - n1])
+                {
+                    swap(arr2[leftIdx - n1], arr2[rightIdx - n1]);
+                }
+            }
+            leftIdx += 1;
+            rightIdx += 1;
+        }
+    }
+
+    void merge(int arr1[], int arr2[], int n1, int n2)
+    {
+
+        int gap = ceil((n1 + n2) / 2);
+
+        int oneCnt = 0;
+
+        while (gap > 0 && oneCnt < 2)
+        {
+            iterateWithGapWidth(arr1, arr2, n1, n2, gap);
+            // printArr(arr1, n1);
+            // printArr(arr2, n2);
+            gap = ceil((double)gap / 2);
+
+            oneCnt += (gap == 1 ? 1 : 0);
+        }
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+
+        cin >> n;
+
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        Solution ob;
+        cout << ob.findDuplicate(arr) << endl;
+    }
+    return 0;
+}
